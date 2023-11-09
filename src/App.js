114a82627d833918f8b4877c493396847344b2fc
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2023-11-09 14:37:15 -0800

    add logic for sending request to face recognition API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ParticlesBg from 'particles-bg'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -7,6 +8,65 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
+
+  const returnClarifaiRequestOptions = (imageUrl) => {
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '2556c2fbcd9041cd9a5a9a2250f426f1';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'ahappswk';       
+    const APP_ID = 'test';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imageUrl;
+   
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+  
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    return requestOptions;
+  
+  }
+
+  const onInputChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  const onButtonSubmit = () => {
+    setImageUrl(input)
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequestOptions(input))
+        .then(response => response.json())
+        .then(response => {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+        })
+        .catch(error => console.log('error', error));
+
+  }
+
   return (
     <div className="App">
       <ParticlesBg 
@@ -18,9 +78,11 @@ function App() {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* 
-      <FaceRecognition /> */}
+      <ImageLinkForm 
+        onInputChange={onInputChange} 
+        onButtonSubmit={onButtonSubmit}
+      />
+      <FaceRecognition imageUrl={imageUrl}/>
     </div>
   );
 }
