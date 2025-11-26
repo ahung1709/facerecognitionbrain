1731fd41be82eb7100260318b75e188c50d02b4c
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2025-11-26 15:17:26 -0800

    update logic for multiple face detection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import ParticlesBg from 'particles-bg'
+import React, { useState } from 'react';
+import ParticlesBg from 'particles-bg';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
@@ -9,27 +9,26 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-const initialInputState = ''
-const initialImageUrlState = ''
-const initialBoxState = {}
-const initialRouteState = 'signin'
-const initialIsSignedInState = false
+const initialInputState = '';
+const initialImageUrlState = '';
+const initialBoxesState = [];
+const initialRouteState = 'signin';
+const initialIsSignedInState = false;
 const initialUserState = {
   id: '',
   name: '',
   email: '',
   entries: 0,
-  joined: ''
-}
+  joined: '',
+};
 
 function App() {
-
-  const [input, setInput] = useState(initialInputState)
-  const [imageUrl, setImageUrl] = useState(initialImageUrlState)
-  const [box, setBox] = useState(initialBoxState)
-  const [route, setRoute] = useState(initialRouteState)
-  const [isSignedIn, setIsSignedIn] = useState(initialIsSignedInState)
-  const [user, setUser] = useState(initialUserState)
+  const [input, setInput] = useState(initialInputState);
+  const [imageUrl, setImageUrl] = useState(initialImageUrlState);
+  const [boxes, setBoxes] = useState(initialBoxesState);
+  const [route, setRoute] = useState(initialRouteState);
+  const [isSignedIn, setIsSignedIn] = useState(initialIsSignedInState);
+  const [user, setUser] = useState(initialUserState);
 
   const loadUser = (data) => {
     setUser({
@@ -37,100 +36,105 @@ function App() {
       name: data.name,
       email: data.email,
       entries: data.entries,
-      joined: data.joined
-    })
-  }
+      joined: data.joined,
+    });
+  };
 
-  const calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width, 
-      topRow: clarifaiFace.top_row * height, 
-      rightCol: width - (clarifaiFace.right_col * width), 
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+  const calculateFaceLocations = (data) => {
+    return data.outputs[0].data.regions.map((face) => {
+      const clarifaiFace = face.region_info.bounding_box;
 
-  const displayFaceBox = (box) => {
-    setBox(box)
-  }
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - clarifaiFace.right_col * width,
+        bottomRow: height - clarifaiFace.bottom_row * height,
+      };
+    });
+  };
+
+  const displayFaceBoxes = (boxes) => {
+    setBoxes(boxes);
+  };
 
   const onInputChange = (event) => {
-    setInput(event.target.value)
-  }
+    setInput(event.target.value);
+  };
 
   const onButtonSubmit = () => {
-    setImageUrl(input)
-        fetch('https://boiling-journey-12073-f29587ce4185.herokuapp.com/imageurl', {
-          method: 'post',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-              input: input
-          })
-        })    
-        .then(response => response.json())
-        .then(response => {
-          if (response) {
-            fetch('https://boiling-journey-12073-f29587ce4185.herokuapp.com/image', {
+    setImageUrl(input);
+    fetch('https://boiling-journey-12073-f29587ce4185.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: input,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response) {
+          fetch(
+            'https://boiling-journey-12073-f29587ce4185.herokuapp.com/image',
+            {
               method: 'put',
-              headers: {'Content-Type': 'application/json'},
+              headers: { 'Content-Type': 'application/json' },
               body: JSON.stringify({
-                  id: user.id
-              })
+                id: user.id,
+              }),
+            }
+          )
+            .then((response) => response.json())
+            .then((count) => {
+              setUser({ ...user, entries: count });
             })
-            .then(response => response.json())
-            .then(count => {
-              setUser({...user, entries: count })
-            })
-            .catch(console.log)
-          }
-          displayFaceBox(calculateFaceLocation(response))
-        }) 
-        .catch(err => console.log(err));
-  }
+            .catch(console.log);
+        }
+        displayFaceBoxes(calculateFaceLocations(response));
+      })
+      .catch((err) => console.log(err));
+  };
 
   const onRouteChange = (route) => {
     if (route === 'signout') {
-      setInput(initialInputState)
-      setImageUrl(initialImageUrlState)
-      setBox(initialBoxState)
-      setRoute(initialRouteState)
-      setIsSignedIn(initialIsSignedInState)
-      setUser(initialUserState)
+      setInput(initialInputState);
+      setImageUrl(initialImageUrlState);
+      setBoxes(initialBoxesState);
+      setRoute(initialRouteState);
+      setIsSignedIn(initialIsSignedInState);
+      setUser(initialUserState);
     } else if (route === 'home') {
-      setIsSignedIn(true)
+      setIsSignedIn(true);
     }
-    setRoute(route)
-  }
+    setRoute(route);
+  };
 
   return (
-    <div className="App">
-      <ParticlesBg 
-        className="particles" 
-        type="cobweb"
-        color="#ffffff" 
-        bg={true} 
+    <div className='App'>
+      <ParticlesBg
+        className='particles'
+        type='cobweb'
+        color='#ffffff'
+        bg={true}
       />
       <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
-      { route === 'home' 
-        ? <div>
-            <Logo />
-            <Rank name={user.name} entries={user.entries} />
-            <ImageLinkForm 
-              onInputChange={onInputChange} 
-              onButtonSubmit={onButtonSubmit}
-            />
-            <FaceRecognition box={box} imageUrl={imageUrl}/>
-          </div>
-        : (
-          route === 'signin' 
-          ? <Signin loadUser={loadUser} onRouteChange={onRouteChange}/>
-          : <Register loadUser={loadUser} onRouteChange={onRouteChange}/>
-        )
-      }
+      {route === 'home' ? (
+        <div>
+          <Logo />
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
+          <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
+        </div>
+      ) : route === 'signin' ? (
+        <Signin loadUser={loadUser} onRouteChange={onRouteChange} />
+      ) : (
+        <Register loadUser={loadUser} onRouteChange={onRouteChange} />
+      )}
     </div>
   );
 }
