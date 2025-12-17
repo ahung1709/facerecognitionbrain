fc20c7a625a2900dd4f364a121a9a679a13dc390
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2025-12-16 19:22:06 -0800

    add logic to fetch to serverless function for generating emoji based on user entries count

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Rank = ({ name, entries }) => {
-    return (
-        <div>
-            <div className='white f3'>
-                {`${name}, your current entry count is...`}
-            </div>
-            <div className='white f1'>
-                {entries}
-            </div>
-        </div>
-    );
-}
+  const [emoji, setEmoji] = useState('');
+
+  const SERVERLESS_BASE_URL = process.env.REACT_APP_SERVERLESS_BASE_URL;
+
+  useEffect(() => {
+    const generateEmoji = (entries) => {
+      fetch(`${SERVERLESS_BASE_URL}/rank?rank=${entries}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setEmoji(data.input);
+        })
+        .catch(console.log);
+    };
+
+    generateEmoji(entries);
+  }, [entries, name, SERVERLESS_BASE_URL]);
+
+  return (
+    <div>
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
+      </div>
+      <div className='white f1'>{entries}</div>
+      <div className='white f3'>{`Rank Badge: ${emoji}`}</div>
+    </div>
+  );
+};
 
 export default Rank;
